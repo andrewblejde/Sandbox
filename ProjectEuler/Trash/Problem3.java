790768dd4fe45fb270191b7e3bf8cf59cd8b5e4f
Andrew Blejde <andrew.blejde@gmail.com>
2018-01-03 19:48:18 -0500

    Don't use int on problem 3

--- ProjectEuler/Trash/Problem3.java
+++ ProjectEuler/Trash/Problem3.java
@@ -3,16 +3,7 @@
  */
  public class Problem3 {
 
- 	public static int gcd(int x, int y) {
- 		if( y == 0 )
- 			return x;
-
- 		return gcd(y, x % y);
- 	}
  	public static void main(String[] args) {
-
- 		// Lol of course, num > Integer.MAX_VALUE
- 		int res = gcd(600851475143);
- 		System.out.println(res);
+ 		
  	}
  }
