7b350c84b511df57153fb331aea73537edd5d07e
Andrew Blejde <andrew.blejde@gmail.com>
2018-01-03 19:51:03 -0500

    q3

--- ProjectEuler/Trash/Problem3.java
+++ ProjectEuler/Trash/Problem3.java
@@ -4,6 +4,15 @@
  public class Problem3 {
 
  	public static void main(String[] args) {
- 		
+ 		Long num = 600851475143L;
+
+ 		int largest = 0;
+
+ 		for(largest = 2; largest <= num; largest++) {
+ 			if(num % largest == 0) 
+ 				num /= largest--;
+ 		}
+
+ 		System.out.println(largest);
  	}
  }
